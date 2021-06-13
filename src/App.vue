8106a72a4d2ea5d331dc834bbf4e9330073fe4bf
Fajrizky <muhammadfajrizky@gmail.com>
2021-06-14 01:30:23 +0700

    Building Single File Components

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    🐥@{{user.username}} - {{fullName}}
+    <strong>Followers : </strong>{{followers}}
+    <button @click="followUser">
+      Follow
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data(){
+    return {
+      followers : 0,
+      user: {
+        id: 1,
+        username: '_Fajrizky',
+        firstName: 'Fajrizky',
+        lastName: 'Diputra',
+        email: 'muhammadfajrizky@gmail.com',
+        isAdmin: true,
+
+      }
+    }
+  },
+  watch: {
+    followers(newFollowCount, oldFollowCount) {
+      if (oldFollowCount < newFollowCount) {
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++
+    }
+  },
+  mounted() {
+    this.followUser();
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
